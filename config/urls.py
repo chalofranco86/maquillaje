7da7935664b5cfa188eca70f7c8540098e7e692a
chalofranco86 <121778441+chalofranco86@users.noreply.github.com>
2025-03-18 14:53:12 -0600

    Update urls.py
    home a las url

--- config/urls.py
+++ config/urls.py
@@ -20,9 +20,11 @@ from django.conf.urls.static import static
 from django.urls import path, include
 from django.conf import settings
 from django.urls import include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('pedidos/', include('pedidos.urls')),
 ]
 
